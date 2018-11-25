88c6fc32f1464057ea6af5bd2e9746da82987806
Mariano Ravinale <mravinale@gmail.com>
2018-11-25 18:47:35 -0300

    Ci integration
    fixes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-express-redirect
+![CircleCI](https://circleci.com/gh/mravinale/redirector/tree/master.svg?style=svg&circle-token=eb3944a0ca7838b583c4f6ed63753a01487ad1ea)
+[![Node version](https://img.shields.io/node/v/[NPM-MODULE-NAME].svg?style=flat)](http://nodejs.org/download/)
+[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/mravinale/redirector/issues)
+# Redirector
+
+## Start application
+
+* start with dev database `yarn start`
+
+* This project is a seed for building a **node.js** api. It includes the following features:
+  * [mocha](https://www.npmjs.com/package/mocha), [supertest](https://www.npmjs.com/package/chai)`integration testing`
--- server.js
+++ server.js
@@ -3,15 +3,14 @@
 /**
  * Module dependencies.
  */
-var app = require('../app');
-var debug = require('debug')('untitled:server');
-var http = require('http');
-
+var app = require("./redirect/app");
+var debug = require("debug")("untitled:server");
+var http = require("http");
 
 /**
  * Normalize a port into a number, string, or false.
  */
-const normalizePort = (val) => {
+const normalizePort = val => {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -30,23 +29,21 @@ const normalizePort = (val) => {
 /**
  * Event listener for HTTP server "error" event.
  */
-const onError = (error) => {
-  if (error.syscall !== 'listen') {
+const onError = error => {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  let bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  let bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -59,18 +56,15 @@ const onError = (error) => {
  */
 const onListening = () => {
   var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
 };
-
 
 /**
  * Get port from environment and store in Express.
  */
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+var port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
 /**
  * Create HTTP server.
@@ -81,7 +75,5 @@ var server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-
+server.on("error", onError);
+server.on("listening", onListening);
