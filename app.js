b2d901651023a521da3cb491f7b0874c9e0fe974
Mariano Ravinale <mravinale@gmail.com>
2018-11-25 17:31:04 -0300

    General refactoring

--- app.js
+++ app.js
@@ -1,47 +1,40 @@
-var express = require('express');
-var bodyParser = require('body-parser')
-var path = require('path');
-var logger = require('morgan');
-var Client = require('node-rest-client').Client;
-var client = new Client();
-
-var indexRouter = require('./routes/index');
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var Client = require("node-rest-client").Client;
 
 var app = express();
-// parse application/json
+var client = new Client();
+
 app.use(bodyParser.json());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//app.use('/', indexRouter);
-const targetBaseUrl = 'http://apidev.lupuscorner.com';
+const targetBaseUrl = "http://apidev.lupuscorner.com";
 //const targetBaseUrl = 'http://localhost:3030';
 
-//client.registerMethod("jsonMethod", targetBaseUrl, "GET");
-
-
-function handleRedirect(req, res) {
-  // res.redirect(targetBaseUrl);
-
-  var args = {
-    data:  req.body, // data passed to REST method (only useful in POST, PUT or PATCH methods)
+const redirectHandler = (req, res) => {
+  let args = {
+    data: req.body,
     headers: {
-      "x-access-token": req.headers["x-access-token"]
-    }// request headers
+      "Content-Type": req.headers["content-type"] || "application/json",
+      "x-access-token": req.headers["x-access-token"] || ""
+    }
   };
 
+  client.registerMethod(
+    "jsonMethod",
+    targetBaseUrl + req.originalUrl,
+    req.method
+  );
 
-  client.registerMethod("jsonMethod", targetBaseUrl+req.originalUrl, req.method);
-
-  client.methods.jsonMethod( args, function (data, response) {
-    // parsed response body as js object
-    console.log(data);
-    res.send(data);
+  client.methods.jsonMethod(args, (data, response) => {
+    let status = response.status || "200";
+    res.status(status).send(data);
   });
+};
 
-}
-app.use('*', handleRedirect);
-
+app.use("*", redirectHandler);
 
 module.exports = app;
